2b42b1c4056f61545886aee72e81d1b46f052a02
Håkon Løvdal <kode@denkule.no>
2025-01-14 18:54:26 +0100

    Refactor: extract functions getDateString and getTimeString

--- display-property.js
+++ display-property.js
@@ -1,3 +1,30 @@
+/**
+ * @param {Date} date
+ * @returns {string}
+ */
+function getDateString(date) {
+	let day = date.getDate();
+	let month = date.getMonth() + 1; // month as a number 0-11, so add 1
+	let year = date.getFullYear();
+	if (day < 10) { day = '0' + day; };
+	if (month < 10) { month = '0' + month; };
+	return year + '-' + month + '-' + day;
+}
+
+/**
+ * @param {Date} date
+ * @returns {string}
+ */
+function getTimeString(date) {
+	let hour = date.getHours();
+	let minute = date.getMinutes();
+	let second = date.getSeconds();
+	if (hour < 10) { hour = '0' + hour; };
+	if (minute < 10) { minute = '0' + minute; }
+	if (second < 10) { second = '0' + second; }
+	return hour + ':' + minute + ':' + second;
+}
+
 module.exports = function (RED) {
 	function DisplayPropertyNode(config) {
 		RED.nodes.createNode(this, config);
@@ -13,22 +40,11 @@ module.exports = function (RED) {
 			const date = new Date();
 			let dateTime = '';
 			if (showDate == 1) {
-				let day = date.getDate();
-				let month = date.getMonth() + 1; // month as a number 0-11, so add 1
-				let year = date.getFullYear();
-				if (day < 10) { day = '0' + day };
-				if (month < 10) { month = '0' + month };
-				dateTime = year + '-' + month + '-' + day;
+				dateTime = getDateString(date);
 			}
 			if (showTime == 1) {
-				if (dateTime != '') { dateTime += ' ' };
-				let hour = date.getHours();
-				let minute = date.getMinutes();
-				let second = date.getSeconds();
-				if (hour < 10) { hour = '0' + hour; };
-				if (minute < 10) { minute = '0' + minute }
-				if (second < 10) { second = '0' + second }
-				dateTime += hour + ':' + minute + ':' + second;
+				const prefix = dateTime != '' ? ' ' : '';
+				dateTime += prefix + getTimeString(date);
 			}
 			if (dateTime != '') { dateTime += ': ' };
 
